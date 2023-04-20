4ce2ccb6fc69098079c5e9ea669337586c0a729e
shulmah <80043404+shulmah@users.noreply.github.com>
2023-04-20 12:38:33 +0300

    Update _test.py

--- _test.py
+++ _test.py
@@ -9,15 +9,6 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLcZXiDHo9IbV7vOtLZ9mtAGnE9DowxOw0go+fsjzE
 n,m= map(int, input().split())
 s = []
 
-for i in range(n):
-    if i % 2 == 0:
-        for j in range(i*m, i*m + m):
-            s.append(j)
-    else:
-        for j in range((i+1)*m -1, i*m -1, -1):
-            s.append(j)
-for i in range(n):
-    print(*s[(i*m): (i*m + m)])
 
 
 # input().split() # целые числа как суб-строки, раздел-ные пробелом 
@@ -55,43 +46,6 @@ s = [   ['*', '*', '*', '.'],
 print()
 sk = [list(map(str,'-'.split()*m)) for i in range(n)]
 
-print('sk=') # PRINT
-for i in range(n):
-    for j in range(m):
-        print(sk[i][j],end=' ')
-    print()
+print('sk=', so) 
 
-for i in range(n): # CHECK -->
-    for j in range(m-1):
-        if s[i][j]=='*':
-            sk[i][j]='0'
-            sk[i][j+1]='0'
-for i in range(n): # CHECK <--
-    for j in range(m-1,0,-1):
-        if s[i][j]=='*':
-            sk[i][j]='0'
-            sk[i][j-1]='0'
-for i in range(n-1): # CHECK DOWN
-    for j in range(m):
-        if s[i][j]=='*':
-            sk[i][j]='0'
-            sk[i+1][j]='0'
-for i in range(n-1,0,-1): # CHECK UP
-    for j in range(m):
-        if s[i][j]=='*':
-            sk[i][j]='0'
-            sk[i-1][j]='0'
 
-print('sk after=')
-for i in range(n):   # PRINT
-    for j in range(m):
-        print(sk[i][j],end=' ')
-    print()
-print()
-
-c=0
-for i in range(n):
-    for j in range(m):
-        if sk[i][j]=='-':
-            c += 1
-print(c)
