716e4cc0159b90261c5f6020aa382486534a5a15
shulmah <shulmah@gmail.com>
2023-04-23 16:34:00 +0300

    0 token 0

--- _test.py
+++ _test.py
@@ -1,50 +1,5 @@
 
-# Программа получает на вход два числа
-# Создать массив  A[n][m] и 
-# заполнить Змейкой
-
-#n,m= map(int, input().split())
-#s = []
+oauth_token: gho_u9gvfeURy6zaI9KaiEqlWKiBvC0QCr2qJwc    user: shulmah
 
 
 
-# input().split() # целые числа как суб-строки, раздел-ные пробелом 
-# n,m = map(int, input().split()) #  ввод 2-х чисел через пробел
-# lst = list(map(int, input().split())) # ввод чисел через проб в списok
-# lst = list(input().split()) # ввод str-чисел через пробел в список
-# matr = [list(map(int, input().split())) for i in range(n)]
-
-# if (w := 'walrus' in input().split()):
-#     print('Нашли моржа')
-# else:
-#     print('Никаких моржей тут нет')
-
-# w = map(str, input().split())
-# if 'walrus' in w:
-#     print('Нашли моржа')
-# else:
-#     print('Никаких моржей тут нет')
-
-# ls = [[0,1,0,1,1],[1,1,0,0,1],[0,0,1,0,1],[1,0,0,0,1],[1,1,1,1,0]]
-# for i in range(len(ls)):
-#     for k in range(len(ls[i])):
-#         print(ls[i][k],end=' ')
-#     print()
-
-#n, m = map(int, input().split())
-#s = [list(map(str, input().split())) for i in range(n)]
-
-n, m = 5 , 4
-s = [   ['*', '*', '*', '.'],
-        ['*', '*', '.', '.'],
-        ['*', '.', '.', '.'],
-        ['*', '.', '.', '.'],
-        ['.', '.', '.', '*']]
-print()
-sk = [list(map(str,'-'.split()*m)) for i in range(n)]
-
-print('sk=', so) 
-
-oauth_token: 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
-
-    user: shulmah
