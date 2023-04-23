1b20a8ab93eb977b1ad38425d38ac9987d19b490
shulmah <80043404+shulmah@users.noreply.github.com>
2023-04-23 13:44:26 +0300

    Update _test.py

--- _test.py
+++ _test.py
@@ -1,10 +1,11 @@
-
 # Программа получает на вход два числа
 # Создать массив  A[n][m] и 
-# заполнить Змейкой
+# заполнить спиралью
 
 n,m= map(int, input('n,m: ').split())
-matr=[]; b=[]; s = []
+matr=[]; c = 1
+y, x = 0, -1
+dy, dx = 0, 1 # step ^ step >
 
 for i in range(n): # create [0]-matrix
     matr.append([0]*m)
@@ -14,6 +15,31 @@ for i in range(n): #print [0]-matrix
         print(matr[i][j], end=' ')
     print()
    
+while c <= n* m:
+  if 0<=y+dy<n and 0<=x+dx<m and matr[y+dy][x+dx]==0:
+    y += dy
+    x += dx
+    matr[y][x] = c
+    c += 1
+  elif dx == 1:
+    dx = 0
+    dy = 1
+  elif dy == 1:
+    dy = 0
+    dx = -1
+  elif dx == -1:
+    dx = 0
+    dy = -1
+  elif dy == -1:
+    dy = 0
+    dx = 1
+
+print() 
+for i in range(n): #print n*m-matrix
+    for j in range(m):
+        print(matr[i][j], end=' ')
+    print()
+
 
 # input().split() # целые числа как суб-строки, раздел-ные пробелом 
 # n,m = map(int, input().split()) #  ввод 2-х чисел через пробел
