5be80079a244fe2af3baa2eaf98cd78c0de001e6
shulmah <80043404+shulmah@users.noreply.github.com>
2023-04-23 13:46:46 +0300

    Update _test.py
    spiral ok

--- _test.py
+++ _test.py
@@ -1,30 +1,45 @@
-
+  
 # Программа получает на вход два числа
 # Создать массив  A[n][m] и 
-# заполнить Змейкой
+# заполнить спиралью
 
 n,m= map(int, input('n,m: ').split())
-matr=[]; b=[]; s = []
+matr=[]; c = 1
+y, x = 0, -1
+dy, dx = 0, 1 # step ^ step >
 
-for i in range(n): # create 0-matrix
+for i in range(n): # create [0]-matrix
     matr.append([0]*m)
     
-for i in range(n): # print 0-matrix
-   for j in range(m):
-       print(matr[i][j], end=' ')
-   print()
-print()
-c=0
-for i in range(n): # added nums to matrix
-   for j in range(m):
-       c+=1
-       if i ==0 and j < m: # add 0-str
-           matr[i][j] = c
-           
-for i in range(n): # print num-matrix
-   for j in range(m):
-       print(matr[i][j], end=' ')
-   print()               
+for i in range(n): #print [0]-matrix
+    for j in range(m):
+        print(matr[i][j], end=' ')
+    print()
+   
+while c <= n* m:
+  if 0<=y+dy<n and 0<=x+dx<m and matr[y+dy][x+dx]==0:
+    y += dy
+    x += dx
+    matr[y][x] = c
+    c += 1
+  elif dx == 1:
+    dx = 0
+    dy = 1
+  elif dy == 1:
+    dy = 0
+    dx = -1
+  elif dx == -1:
+    dx = 0
+    dy = -1
+  elif dy == -1:
+    dy = 0
+    dx = 1
+
+print() 
+for i in range(n): #print n*m-matrix
+    for j in range(m):
+        print(matr[i][j], end=' ')
+    print()     
                
            
 
